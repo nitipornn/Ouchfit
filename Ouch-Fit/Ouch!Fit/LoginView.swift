63b9f6fc87c0aff3122b6b0b4b9b86d615b50256
mminggming <mmminggg13gmail.com>
2025-05-11 00:53:57 +0700

    test

---
+++ Ouch-Fit/Ouch!Fit/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Ouch!Fit
+//
+//  Created by Chanita Pornsaktawee on 11/5/2568 BE.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
