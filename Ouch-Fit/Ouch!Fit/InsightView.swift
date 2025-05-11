f11b2228f9eb0e3a4aa4f0313674e6c8ba68d254
nitiporn <eclair.ni@gmail.com>
2025-05-12 02:04:42 +0700

    7

---
+++ Ouch-Fit/Ouch!Fit/InsightView.swift
@@ -0,0 +1,18 @@
+//
+//  InsightView.swift
+//  Ouch!Fit
+//
+//  Created by Nitiporn Siriwimonwan on 12/5/2568 BE.
+//
+
+import SwiftUI
+
+struct InsightView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InsightView()
+}
---
+++ Ouch-Fit/Ouch!Fit/UploadItemView.swift
@@ -0,0 +1,25 @@
+//
+//  uploaditems.swift
+//  Ouch!Fit
+//
+//  Created by Nitiporn Siriwimonwan on 12/5/2568 BE.
+//
+
+import SwiftUI
+
+struct UploadItemView: View {
+    var body: some View {
+        VStack {
+            Text("This is the Uploaditems Page")
+                .font(.largeTitle)
+                .bold()
+                .padding()
+
+            Spacer()
+        }
+        .navigationTitle("Uploaditems")
+    }
+}
+#Preview {
+    UploadItemView()
+}
