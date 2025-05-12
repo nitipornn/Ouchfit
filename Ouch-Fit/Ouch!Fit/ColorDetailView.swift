b838d9b1b3c22c6e17456e3659705f9eac63753c
nitiporn <eclair.ni@gmail.com>
2025-05-12 20:57:00 +0700

    7

---
+++ Ouch-Fit/Ouch!Fit/ColorDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorDetailView.swift
+//  Ouch!Fit
+//
+//  Created by Nitiporn Siriwimonwan on 12/5/2568 BE.
+//
+
+import SwiftUI
+
+struct ColorDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorDetailView()
+}
---
+++ Ouch-Fit/SizeDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  SizeDetailsView.swift
+//  Ouch!Fit
+//
+//  Created by Nitiporn Siriwimonwan on 12/5/2568 BE.
+//
+
+import SwiftUI
+
+struct SizeDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SizeDetailsView()
+}
