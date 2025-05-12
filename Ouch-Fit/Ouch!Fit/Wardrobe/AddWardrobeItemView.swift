a64ebc335cf0006ca603c5ff41af4680e2a9f541
mminggming <mmminggg13gmail.com>
2025-05-13 01:02:04 +0700

    m

--- Ouch-Fit/Ouch!Fit/Wardrobe/AddWardrobeItemView.swift
+++ Ouch-Fit/Ouch!Fit/Wardrobe/AddWardrobeItemView.swift
@@ -10,7 +10,7 @@ struct AddWardrobeItemView: View {
     @State private var size: String = ""
     @State private var datePurchased: Date = Date()
     @State private var season: String = ""
-    @State private var location: [String] = [] // Location list
+    @State private var location: String = "" // Location should be a single value (not an array)
     @State private var fabric: String = "" // Default value for fabric
     @State private var newLocation: String = "" // For adding new location
     
@@ -26,119 +26,140 @@ struct AddWardrobeItemView: View {
     @Environment(\.presentationMode) var presentationMode // To dismiss after saving
 
     // Predefined locations
-    let predefinedLocations = ["Home","Condo"]
+    let predefinedLocations = ["Home", "Condo", "Office"]
 
     var body: some View {
-        Form {
-            Section(header: Text("Item Details")) {
-                TextField("Name", text: $name)
-                
-                // Color Picker
-                Picker("Color", selection: $color) {
-                    ForEach(availableColors, id: \.self) { color in
-                        Text(color).tag(color)
-                    }
-                }
-                .pickerStyle(MenuPickerStyle())
-                .padding(.vertical)
+        NavigationView {
+            Form {
+                // Item details section
+                Section(header: Text("Item Details")) {
+                    TextField("Name", text: $name)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
 
-                // Fabric Picker
-                Picker("Fabric", selection: $fabric) {
-                    ForEach(availableFabrics, id: \.self) { fabric in
-                        Text(fabric).tag(fabric)
+                    // Color Picker
+                    Picker("Color", selection: $color) {
+                        ForEach(availableColors, id: \.self) { color in
+                            Text(color).tag(color)
+                        }
                     }
-                }
-                .pickerStyle(MenuPickerStyle())
-                .padding(.vertical)
-                
-                TextField("Brand", text: $brand)
-                TextField("Size", text: $size)
-                
-                // Price Field
-                TextField("Price", text: $price)
-                    .keyboardType(.decimalPad) // Ensure price is a number (e.g., 99.99)
+                    .pickerStyle(MenuPickerStyle())
+                    .padding(.vertical)
 
-                Picker("Category", selection: $category) {
-                    Text("Shirt").tag("Shirt")
-                    Text("Pants").tag("Pants")
-                    // Add more categories as needed
-                }
-                
-                Picker("Season", selection: $season) {
-                    Text("Summer").tag("Summer")
-                    Text("Winter").tag("Winter")
-                    // Add more seasons if needed
-                }
-                
-                DatePicker("Date Purchased", selection: $datePurchased, displayedComponents: .date)
-            }
+                    // Fabric Picker
+                    Picker("Fabric", selection: $fabric) {
+                        ForEach(availableFabrics, id: \.self) { fabric in
+                            Text(fabric).tag(fabric)
+                        }
+                    }
+                    .pickerStyle(MenuPickerStyle())
+                    .padding(.vertical)
 
-            Section(header: Text("Location")) {
-                // Predefined Locations Picker
-                Picker("Select a Location", selection: $location) {
-                    ForEach(predefinedLocations, id: \.self) { location in
-                        Text(location).tag(location)
-                    }
-                }
-                .pickerStyle(MenuPickerStyle())
-                
-                // TextField to add a new custom location
-                TextField("Add a custom location", text: $newLocation)
-                
-                Button(action: {
-                    if !newLocation.isEmpty {
-                        location.append(newLocation)
-                        newLocation = "" // Clear the text field after adding
-                    }
-                }) {
-                    Text("Add Location")
-                        .font(.body)
-                        .foregroundColor(.blue)
-                }
-                .padding(.top, 10)
-            }
+                    TextField("Brand", text: $brand)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    TextField("Size", text: $size)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
 
-            Section(header: Text("Image")) {
-                VStack {
-                    // Display the selected image if it exists
-                    if let selectedImage = selectedImage {
-                        Image(uiImage: selectedImage)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 200, height: 200)
-                    }
-                    Button("Pick Image") {
-                        isImagePickerPresented.toggle()
-                    }
-                    .fullScreenCover(isPresented: $isImagePickerPresented) {
-                        ImagePicker(selectedImage: $selectedImage, isImagePickerPresented: $isImagePickerPresented)
-                    }
-                }
-            }
+                    // Price Field
+                    TextField("Price", text: $price)
+                        .keyboardType(.decimalPad)
+                        .textFieldStyle(RoundedBorderTextFieldStyle()) // Ensure price is a number (e.g., 99.99)
 
-            Section {
-                Button("Save") {
-                    // If the image is selected, convert it to a base64 string to store it
-                    let imageData = selectedImage?.jpegData(compressionQuality: 1.0)?.base64EncodedString() ?? ""
+                    Picker("Category", selection: $category) {
+                        Text("Shirt").tag("Shirt")
+                        Text("Pants").tag("Pants")
+                        // Add more categories as needed
+                    }
+
+                    Picker("Season", selection: $season) {
+                        Text("Summer").tag("Summer")
+                        Text("Winter").tag("Winter")
+                        // Add more seasons if needed
+                    }
+
+                    DatePicker("Date Purchased", selection: $datePurchased, displayedComponents: .date)
+                }
+
+                // Location section (single selection)
+                Section(header: Text("Location")) {
+                    // Predefined Locations Picker (single selection)
+                    Picker("Select Location", selection: $location) {
+                        ForEach(predefinedLocations, id: \.self) { location in
+                            Text(location).tag(location)
+                        }
+                    }
+                    .pickerStyle(MenuPickerStyle())
                     
-                    // Convert the price string to a double
-                    let priceValue = Double(price) ?? 0.0
-                    
-                    let newItem = WardrobeItem(
-                        name: name, color: color, category: category,
-                        brand: brand, price: priceValue, size: size,
-                        datePurchased: datePurchased, season: season,
-                        location: location, fabric: fabric, imageURL: imageData
-                    )
-                    
-                    viewModel.addItem(item: newItem)
-                    
-                    // Dismiss the current view after saving
-                    presentationMode.wrappedValue.dismiss()
+                    // TextField to add a new custom location
+                    TextField("Add a custom location", text: $newLocation)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.top, 10)
+
+                    // Button to add custom location to the list
+                    Button(action: {
+                        if !newLocation.isEmpty {
+                            location = newLocation // Update selected location with the new custom location
+                            newLocation = "" // Clear the text field after adding
+                        }
+                    }) {
+                        Text("Add Location")
+                            .font(.body)
+                            .foregroundColor(.blue)
+                    }
+                    .padding(.top, 10)
+                }
+
+                // Image section
+                Section(header: Text("Image")) {
+                    VStack {
+                        // Display the selected image if it exists
+                        if let selectedImage = selectedImage {
+                            Image(uiImage: selectedImage)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 200, height: 200)
+                        }
+
+                        Button("Pick Image") {
+                            isImagePickerPresented.toggle()
+                        }
+                        .fullScreenCover(isPresented: $isImagePickerPresented) {
+                            ImagePicker(selectedImage: $selectedImage, isImagePickerPresented: $isImagePickerPresented)
+                        }
+                    }
+                }
+
+                // Save Button
+                Section {
+                    Button("Save") {
+                        // If the image is selected, convert it to a base64 string to store it
+                        let imageData = selectedImage?.jpegData(compressionQuality: 1.0)?.base64EncodedString() ?? ""
+                        
+                        // Convert the price string to a double
+                        let priceValue = Double(price) ?? 0.0
+                        
+                        let newItem = WardrobeItem(
+                            name: name, color: color, category: category,
+                            brand: brand, price: priceValue, size: size,
+                            datePurchased: datePurchased, season: season,
+                            location: [location], fabric: fabric, imageURL: imageData
+                        )
+                        
+                        viewModel.addItem(item: newItem)
+                        
+                        // Dismiss the current view after saving
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding()
+                    .background(Color.black)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
                 }
             }
+            .navigationBarTitle("Add Wardrobe Item")
+            .background(Color.white) // Set background to white
+            .foregroundColor(.black) // Set text to black
         }
-        .navigationTitle("Add Wardrobe Item")
     }
 }
 
