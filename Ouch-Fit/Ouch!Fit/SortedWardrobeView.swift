640def1c8d1ab502a7a1952a18c34b289f801fb8
nitiporn <eclair.ni@gmail.com>
2025-05-13 03:24:52 +0700

    9

---
+++ Ouch-Fit/Ouch!Fit/SortedWardrobeView.swift
@@ -0,0 +1,18 @@
+//
+//  SortedWardrobeView.swift
+//  Ouch!Fit
+//
+//  Created by Nitiporn Siriwimonwan on 13/5/2568 BE.
+//
+
+import SwiftUI
+
+struct SortedWardrobeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SortedWardrobeView()
+}
